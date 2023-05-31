472052c49e3c6b51dd7c5be7d944640690d252fd
Thales <thales.navasques@gmail.com>
2023-05-31 14:25:52 -0300

    :sparkles: feat: Inclusão metodos PUT e DELETE

--- src/main/java/br/com/api/projeto/controller/ClienteController.java
+++ src/main/java/br/com/api/projeto/controller/ClienteController.java
@@ -1,8 +1,11 @@
 package br.com.api.projeto.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +26,15 @@ public class ClienteController {
     @GetMapping("/")
     public Iterable<Cliente> listar() {
         return acao.findAll();
+    }
+
+    @PutMapping("/")
+    public Cliente editar(@RequestBody Cliente c) {
+        return acao.save(c);
+    }
+
+    @DeleteMapping("/{codigo}")
+    public void remover(@PathVariable long codigo) {
+        acao.deleteById(codigo);
     }
 }
