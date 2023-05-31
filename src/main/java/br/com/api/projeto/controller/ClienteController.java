742fd254cd19bbba65c2095170425e25b2b33075
Thales <thales.navasques@gmail.com>
2023-05-31 13:40:58 -0300

    :sparkles: feat: Inclisão metodo POST Cadastrar
    :sparkles: feat: Inclusão metodo POST Cadastrar

--- src/main/java/br/com/api/projeto/controller/ClienteController.java
+++ src/main/java/br/com/api/projeto/controller/ClienteController.java
@@ -1,10 +1,24 @@
 package br.com.api.projeto.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.projeto.model.Cliente;
+import br.com.api.projeto.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
+
+    @Autowired
+    private ClienteRepository acao;
+
+    @PostMapping("/")
+    public Cliente cadastrar(@RequestBody Cliente c) {
+        return acao.save(c);
+    }
 
     @GetMapping("/")
     public String teste() {
