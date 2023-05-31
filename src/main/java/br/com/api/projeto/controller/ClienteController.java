b790f7d4e6fcea2ec037338d06cfa17018723d6f
Thales <thales.navasques@gmail.com>
2023-05-31 13:18:24 -0300

    :recycle: refactor: Inclusão metodo GET

--- src/main/java/br/com/api/projeto/controller/ClienteController.java
+++ src/main/java/br/com/api/projeto/controller/ClienteController.java
@@ -1,5 +1,13 @@
 package br.com.api.projeto.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ClienteController {
-    
+
+    @GetMapping("/")
+    public String teste() {
+        return "Hello World!";
+    }
 }
