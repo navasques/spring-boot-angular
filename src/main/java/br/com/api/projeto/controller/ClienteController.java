54b0e6869a88bdb6c18223a23e42dd194ccd89ed
Thales <thales.navasques@gmail.com>
2023-05-31 14:06:41 -0300

    :sparkles: feat: Inclusão metodo GET Listar Clientes

--- src/main/java/br/com/api/projeto/controller/ClienteController.java
+++ src/main/java/br/com/api/projeto/controller/ClienteController.java
@@ -1,6 +1,8 @@
 package br.com.api.projeto.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,7 +23,7 @@ public class ClienteController {
     }
 
     @GetMapping("/")
-    public String teste() {
-        return "Hello World!";
+    public Iterable<Cliente> listar() {
+        return acao.findAll();
     }
 }
