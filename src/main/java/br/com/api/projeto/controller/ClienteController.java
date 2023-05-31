3a57ac567b21891acd80ad69c6dffecbf590efb8
Thales <thales.navasques@gmail.com>
2023-05-31 14:47:19 -0300

    :recycle: feat: Inclusão da anotação 'CrossOrigin'

--- src/main/java/br/com/api/projeto/controller/ClienteController.java
+++ src/main/java/br/com/api/projeto/controller/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.api.projeto.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import br.com.api.projeto.model.Cliente;
 import br.com.api.projeto.repository.ClienteRepository;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ClienteController {
 
     @Autowired
