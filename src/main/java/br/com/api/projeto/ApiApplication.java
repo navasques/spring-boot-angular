411114d6f2ed28221c9a95af6b702e2563246528
Thales <thales.navasques@gmail.com>
2023-05-31 12:51:54 -0300

    :recycle: refactor: Alteração nome para 'ApiApplication'

--- src/main/java/br/com/api/projeto/ApiApplication.java
+++ src/main/java/br/com/api/projeto/ApiApplication.java
@@ -1,13 +1,13 @@
-package br.com.api.produtos;
+package br.com.api.projeto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProdutosApplication {
+public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProdutosApplication.class, args);
+		SpringApplication.run(ApiApplication.class, args);
 	}
 
 }
